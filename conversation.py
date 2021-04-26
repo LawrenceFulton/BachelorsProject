e5abdef8eeac6c7a827063f02bff5159ef55662b
LawrenceFulton <lawrence@fulton.de>
2021-04-26 15:21:40 +0200

    addepted code for batch approach

--- conversation.py
+++ conversation.py
@@ -109,8 +109,8 @@ def voting_alg(underlying_opinion):
     # the total disussions which can be proposed
     n_votes = underlying_opinion.shape[1]
     
-    vote_dist, sum_vote = get_distribution(n_participant, n_votes/4)
-    person_list = list(range(n_participant))
+    vote_dist, sum_vote = get_distribution(n_participant-n_admins, n_votes/4)
+    person_list = list(range(n_admins, n_participant))
 
 
 
@@ -142,8 +142,6 @@ def voting_alg(underlying_opinion):
                 
             has_seen[admin,vote] = True
             decision = underlying_opinion[admin,vote]
-            # decision = rd.choice([True,False])
-
             known_votes[admin,vote] = decision
             if decision == ACCEPT:
                 A[vote] += 1
@@ -165,107 +163,59 @@ def voting_alg(underlying_opinion):
             # update for the cmd 
             print("index", i)
         
-        # the number of known people
-        n_known_people = known_votes.shape[0]
-        # the number of known votes
-        n_known_votes = known_votes.shape[1] ## the open question
-
 
         ### Choice of person
 
         # picks a random person from the whole data
-        # rand_per = rd.randint(0,n_known_people)
         rand_per = np.random.choice( person_list, 1, p = vote_dist)
         
         ### Choice of question
         
         chosen_question = -99
         # a dynamic probability which will make people propose new questions one in n_known_votes times
-        rand_vote = rd.randint(0,n_known_votes)
-        if rand_vote == n_known_votes:
-            
-            # stopping condition so we will stick at the amount of questions people have 
-            if rand_vote == n_votes: 
-                continue
-            # we append the column into the known database 
-            question_to_append = np.zeros([n_known_people,1],dtype=bool)
-            
-            # appending the new empty data to the known data
-            known_votes = np.c_[known_votes,question_to_append]
-            has_seen = np.c_[has_seen,question_to_append]
-    
-    
-            ## get the actual vote from the underlying knowledgebase
-            vote = underlying_opinion[rand_per,rand_vote]
-            # updating the has_voted matrix
-            has_seen[rand_per,rand_vote] = True
 
-            
-            n_known_votes += 1
-            chosen_question = rand_vote
+        E = get_e(known_votes)
+        priority = priority_metric(A,P,S,E)
+        p_has_seen = has_seen[rand_per,:][0] # mask for the current person
+
+        if np.all(p_has_seen):
+            # deals with the case that all questions are already seen by this person
+            continue
+
+        # cleaning priority so that no question will be proposed which the user has already seen
+        cleaned_priority = priority[~p_has_seen]
+
+        if sum(cleaned_priority) == 0:
+            print("all questions already answered for person ", rand_per)
+            continue
+        # chosen_question_filtered = get_max(cleaned_priority)
+        chosen_question_filtered = get_probability(cleaned_priority)
+        # print(chosen_question_filtered)
 
 
+        # allows us to get back from the cleaned priority to all real values
+        chosen_question = -99
+        for j in range(len(p_has_seen)):
+            if p_has_seen[j] == False:
+                chosen_question_filtered -= 1
+                if chosen_question_filtered == -1:
+                    chosen_question = j
 
-            # updating P,S,A
-            S.append(1)
-            if vote == ACCEPT:
-                A.append(1)
-                P.append(0)
-            elif vote == PASS:
-                P.append(1)
-                A.append(0)
-            elif vote == -1:
-                P.append(0)
-                A.append(0)
-            else:
-                print("vote is ", vote)
-                print("this should not happen please investigate")
-                sys.exit(0)
-                # return consensus
-
-        else:
-
-            E = get_e(known_votes)
-            priority = priority_metric(A,P,S,E)
-            p_has_seen = has_seen[rand_per,:][0] # mask for the current person
-
-            if np.all(p_has_seen):
-                # deals with the case that all questions are already seen by this person
-                continue
-
-            # cleaning priority so that no question will be proposed which the user has already seen
-            cleaned_priority = priority[~p_has_seen]
-
-            if sum(cleaned_priority) == 0:
-                continue
-            # chosen_question_filtered = get_max(cleaned_priority)
-            chosen_question_filtered = get_probability(cleaned_priority)
-            # print(chosen_question_filtered)
+        if chosen_question == -99:
+            print("some mistake has happend")
+            sys.exit(0)
 
 
-            # allows us to get back from the cleaned priority to all real values
-            chosen_question = -99
-            for j in range(len(p_has_seen)):
-                if p_has_seen[j] == False:
-                    chosen_question_filtered -= 1
-                    if chosen_question_filtered == -1:
-                        chosen_question = j
+        vote = underlying_opinion[rand_per, chosen_question]
 
-            if chosen_question == -99:
-                print("sdjabdjkabsdjkabdhasb")
-                sys.exit(0)
+        known_votes[rand_per,chosen_question] = vote
+        has_seen[rand_per,chosen_question] = True
 
-
-            vote = underlying_opinion[rand_per, chosen_question]
-
-            known_votes[rand_per,chosen_question] = vote
-            has_seen[rand_per,chosen_question] = True
-
-            # updated the A and P values
-            if vote == ACCEPT:
-                A[chosen_question] += 1
-            elif vote == PASS:
-                P[chosen_question] += 1 
+        # updated the A and P values
+        if vote == ACCEPT:
+            A[chosen_question] += 1
+        elif vote == PASS:
+            P[chosen_question] += 1 
         
         ## appending the vote into a db which can then be analysed 
         new_item = np.array([chosen_question,rand_per[0], vote[0]])
@@ -278,7 +228,7 @@ def voting_alg(underlying_opinion):
     vote_hist = np.delete(vote_hist, (0), axis=0)
 
 
-
+    ## saving the data for further analysis
     print("somehow finished:")
     print("known_votes,", known_votes)
     pd.DataFrame(known_votes).to_csv('data/known_votes.csv')
