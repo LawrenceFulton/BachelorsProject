e75d4b8b9cdb78bf2e1a70ea95c582533bc3333e
LawrenceFulton <lawrence@fulton.de>
2021-04-29 16:19:49 +0200

    added a Eps greedy approach on the comment roting

--- conversation.py
+++ conversation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.fromnumeric import argmax
 from numpy.linalg import norm
 from sklearn.decomposition import PCA
 import pandas as pd
@@ -8,6 +9,10 @@ import sys
 
 PASS = 0
 ACCEPT = 1
+
+POLIS = 0
+EPS = 1
+
 
 def data_creation(n_indi, n_decision, n_proto = 2):
     cutoff = 0.07 # pretty much arbitrarily chosen 
@@ -131,8 +136,39 @@ def get_comment_polis(known_votes, P, A, S, has_seen, cur_per ):
 
     return chosen_comment
 
+def get_comment_eps(has_seen, cur_per, action_value, eps):
+    '''
+    comment routing based on epsylon greedy 
+    '''
+    p_has_seen = has_seen[cur_per,:][0]
+    if np.all(p_has_seen):
+        return -1
 
-def voting_alg(underlying_opinion):
+    cleaned_action_value = action_value[~p_has_seen]
+
+    if sum(cleaned_action_value) == 0:
+        return -1
+
+    if rd.random() > eps:
+        chosen_comment_filtered = np.argmax(cleaned_action_value)
+    else:
+        chosen_comment_filtered = rd.randint(0,len(cleaned_action_value)-1)
+
+    # allows us to get back from the cleaned priority to all real values
+    chosen_comment = -99
+    for j in range(len(p_has_seen)):
+        if p_has_seen[j] == False:
+            chosen_comment_filtered -= 1
+            if chosen_comment_filtered == -1:
+                chosen_comment = j
+
+    if chosen_comment == -99:
+        print("some mistake has happend")
+        sys.exit(0)
+
+    return chosen_comment
+
+def voting_alg(underlying_opinion, comment_routing):
     # the number of participants    
     n_participant = underlying_opinion.shape[0]
     
@@ -146,7 +182,7 @@ def voting_alg(underlying_opinion):
     
     # vote_dist gives the likellyhood for each comment to appear
     # sum_votes gives the total amount of votes we want to investigate
-    vote_dist, sum_vote = get_distribution(n_participant-n_admins, n_votes)
+    vote_dist, sum_vote = get_distribution(n_participant-n_admins, n_votes / 10)
     person_list = list(range(n_admins, n_participant))
 
 
@@ -164,6 +200,11 @@ def voting_alg(underlying_opinion):
     P = [0] * n_votes
     A = [0] * n_votes
     S = [0] * n_votes
+
+
+    # needed for EPS
+    eps = 0.2
+    action_value = np.random.uniform(-0.1, 0.1, n_votes)
 
 
     # init phase
@@ -210,22 +251,36 @@ def voting_alg(underlying_opinion):
         chosen_comment = -99
         # a dynamic probability which will make people propose new questions one in n_known_votes times
 
-        chosen_comment = get_comment_polis(known_votes, P, A, S, has_seen, cur_per)
+        if comment_routing == POLIS:
+            chosen_comment = get_comment_polis(known_votes, P, A, S, has_seen, cur_per)
 
+        elif comment_routing == EPS:
+            chosen_comment = get_comment_eps(has_seen, cur_per, action_value, eps )
 
+        else: 
+            print("error")
+            sys.exit(0)
 
-
+        if chosen_comment == -1:
+            # deals with all errors where one shall skip a question, such as a person has seen each question 
+            continue
 
         vote = underlying_opinion[cur_per, chosen_comment]
 
         known_votes[cur_per,chosen_comment] = vote
         has_seen[cur_per,chosen_comment] = True
 
-        # updated the A and P values
+        # updated the A / P / S values
         if vote == ACCEPT:
             A[chosen_comment] += 1
         elif vote == PASS:
             P[chosen_comment] += 1 
+
+        S[chosen_comment] += 1
+
+        if comment_routing == EPS:
+            # have to update the action value 
+            action_value[chosen_comment] =  1 / (S[chosen_comment]) * (vote - action_value[chosen_comment])
         
         ## appending the vote into a db which can then be analysed 
         new_entry = np.array([chosen_comment, cur_per[0], vote[0]])
@@ -241,15 +296,15 @@ def voting_alg(underlying_opinion):
     ## saving the data for further analysis
     print("somehow finished:")
     print("known_votes,", known_votes)
-    pd.DataFrame(known_votes).to_csv('data/known_votes_45.csv')
+    pd.DataFrame(known_votes).to_csv('data/known_votes.csv')
     pd.DataFrame(has_seen).to_csv('data/has_seen.csv')
-    pd.DataFrame(vote_hist).to_csv('data/vote_hist.csv')
+    pd.DataFrame(vote_hist).to_csv('data/vote_hist_52.csv')
 
 
 if __name__ == "__main__":
-    n_indi = 116
-    n_votes = 156 # number of different votes
+    n_indi = 1163
+    n_votes = 156 * 10 # number of different votes
     data = data_creation(n_indi, n_votes, 5)
 
 
-    consensus = voting_alg(data)
+    consensus = voting_alg(data, EPS)
