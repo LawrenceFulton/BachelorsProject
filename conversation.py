d2f0fcd657bba6c4e1b30bbf015ee8352d407b79
LawrenceFulton <lawrence@fulton.de>
2021-04-29 13:42:37 +0200

    updated the batch approach, maybe created a mix bettween the two approaches

--- conversation.py
+++ conversation.py
@@ -54,9 +54,8 @@ def data_creation(n_indi, n_decision, n_proto = 2):
     
 
 def get_distribution(n_indi, n_decision):
-    # dist = voting_distribution(n_indi, n_decision)
-    # sum_vote = sum(dist)
-    # per_dist = dist / sum(dist)
+    print(n_indi, n_decision)
+
     ratio = n_decision / n_indi
     y = []
     cur_y = n_decision
@@ -64,15 +63,20 @@ def get_distribution(n_indi, n_decision):
         y.append(int(cur_y))
         cur_y -= ratio
     
-
-    
     sum_vote = sum(y)
     per_dist = np.array(y) / sum_vote
 
+    print("sum_votes: ", sum_vote)
 
     return per_dist, sum_vote 
 
 def priority_metric(A,P,S,E):
+    '''
+    A: Accepted
+    P: Passed
+    S: Seen
+    E: Distance (from center in pca reduced space)
+    '''
     A = np.array(A)
     P = np.array(P)
     S = np.array(S)
@@ -96,37 +100,37 @@ def get_probability(vector):
 
 
 def voting_alg(underlying_opinion):
-    ## variables 
-
-    # the number of admins 
-    n_admins = 2
-    # the number of votes the admins create and also vote on
-    n_pregiven_votes = underlying_opinion.shape[1] - 1
-    
-    # the number of participants
+    # the number of participants    
     n_participant = underlying_opinion.shape[0]
     
     # the total disussions which can be proposed
     n_votes = underlying_opinion.shape[1]
+
+    # the number of admins 
+    n_admins = 1
+    # the number of votes the admins create and also vote on
+    n_pregiven_votes = 5
     
-    vote_dist, sum_vote = get_distribution(n_participant-n_admins, n_votes/4)
+    # vote_dist gives the likellyhood for each comment to appear
+    # sum_votes gives the total amount of votes we want to investigate
+    vote_dist, sum_vote = get_distribution(n_participant-n_admins, n_votes/10)
     person_list = list(range(n_admins, n_participant))
 
 
 
     # the votes which are available 
-    known_votes = np.zeros([n_participant,n_pregiven_votes])
+    known_votes = np.zeros([n_participant,n_votes])
     # if a user has seen a particular question 
-    has_seen = np.zeros([n_participant,n_pregiven_votes],dtype='bool')
+    has_seen = np.zeros([n_participant,n_votes],dtype='bool')
     
 
     # the vote history, which at some point can be imported to continuosConsensus.py
     vote_hist = np.zeros((1,3))
 
-    # Pass / Accepted / Seen, taken from the original closure code, will continuously be updated 
-    P = [0] * n_pregiven_votes
-    A = [0] * n_pregiven_votes
-    S = [n_admins] * n_pregiven_votes
+    # Pass / Accepted / Seen, taken from the original clj code, will continuously be updated 
+    P = [0] * n_votes
+    A = [0] * n_votes
+    S = [0] * n_votes
 
 
     # init phase
@@ -138,8 +142,7 @@ def voting_alg(underlying_opinion):
     for admin in range(n_admins):
         for vote in range(n_pregiven_votes):
 
-            # if rd.choice([True,False]):
-                
+
             has_seen[admin,vote] = True
             decision = underlying_opinion[admin,vote]
             known_votes[admin,vote] = decision
@@ -147,14 +150,14 @@ def voting_alg(underlying_opinion):
                 A[vote] += 1
             if decision == PASS:
                 P[vote] += 1
+            S[vote] += 1
 
 
-            new_item = np.array([vote,admin, decision])
-            new_item = new_item.reshape(1,3)
+            # new_item = np.array([vote,admin, decision])
+            # new_item = new_item.reshape(1,3)
 
-            vote_hist = np.append(vote_hist,new_item, axis=0)
+            # vote_hist = np.append(vote_hist,new_item, axis=0)
                 
-
 
 
     ## only half of the possible votes can be decided on
@@ -167,16 +170,16 @@ def voting_alg(underlying_opinion):
         ### Choice of person
 
         # picks a random person from the whole data
-        rand_per = np.random.choice( person_list, 1, p = vote_dist)
+        cur_per = np.random.choice( person_list, 1, p = vote_dist)
         
         ### Choice of question
         
-        chosen_question = -999
+        chosen_question = -99
         # a dynamic probability which will make people propose new questions one in n_known_votes times
 
         E = get_e(known_votes)
         priority = priority_metric(A,P,S,E)
-        p_has_seen = has_seen[rand_per,:][0] # mask for the current person
+        p_has_seen = has_seen[cur_per,:][0] # mask for the current person
 
         if np.all(p_has_seen):
             # deals with the case that all questions are already seen by this person
@@ -186,7 +189,7 @@ def voting_alg(underlying_opinion):
         cleaned_priority = priority[~p_has_seen]
 
         if sum(cleaned_priority) == 0:
-            print("all questions already answered for person ", rand_per)
+            # print("all questions already answered for person ", cur_per)
             continue
         # chosen_question_filtered = get_max(cleaned_priority)
         chosen_question_filtered = get_probability(cleaned_priority)
@@ -206,10 +209,10 @@ def voting_alg(underlying_opinion):
             sys.exit(0)
 
 
-        vote = underlying_opinion[rand_per, chosen_question]
+        vote = underlying_opinion[cur_per, chosen_question]
 
-        known_votes[rand_per,chosen_question] = vote
-        has_seen[rand_per,chosen_question] = True
+        known_votes[cur_per,chosen_question] = vote
+        has_seen[cur_per,chosen_question] = True
 
         # updated the A and P values
         if vote == ACCEPT:
@@ -218,7 +221,7 @@ def voting_alg(underlying_opinion):
             P[chosen_question] += 1 
         
         ## appending the vote into a db which can then be analysed 
-        new_item = np.array([chosen_question,rand_per[0], vote[0]])
+        new_item = np.array([chosen_question, cur_per[0], vote[0]])
         new_item = new_item.reshape(1,3)
 
         vote_hist = np.append(vote_hist,new_item, axis=0)
@@ -231,15 +234,15 @@ def voting_alg(underlying_opinion):
     ## saving the data for further analysis
     print("somehow finished:")
     print("known_votes,", known_votes)
-    pd.DataFrame(known_votes).to_csv('data/known_votes.csv')
+    pd.DataFrame(known_votes).to_csv('data/known_votes_45.csv')
     pd.DataFrame(has_seen).to_csv('data/has_seen.csv')
     pd.DataFrame(vote_hist).to_csv('data/vote_hist.csv')
 
 
 if __name__ == "__main__":
-    n_indi = 70
-    n_votes = 540 # number of different votes
-    data = data_creation(n_indi, n_votes,5)
+    n_indi = 1163
+    n_votes = 156 * 10 # number of different votes
+    data = data_creation(n_indi, n_votes, 5)
 
 
     consensus = voting_alg(data)
