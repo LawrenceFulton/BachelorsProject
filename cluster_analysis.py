9fec37e3f0f7668b7e66c9bb7166963aa3b798f3
LawrenceFulton <lawrence@fulton.de>
2021-06-04 09:00:57 +0200

    added a complete cleaning process into the preprocessing

--- cluster_analysis.py
+++ cluster_analysis.py
@@ -1,4 +1,5 @@
 from numpy.core.fromnumeric import shape
+from numpy.random import randint
 from conversation import data_creation
 from repness import prop_pos
 import numpy as np
@@ -8,52 +9,18 @@ from sklearn.metrics import cluster
 import continousConsensus as cc
 from sklearn.metrics import silhouette_score
 import repness as rep
-
-def fetch_data():
-
-
-    df = pd.read_csv("data/votes.csv")
-    print(df)
-
-    df = df.sort_values(by = 'timestamp')
-    df = df.drop(['datetime'], axis = 1)
-    df = df.drop(['timestamp'], axis = 1)
-
-    df = df.values
-    return df
+from sklearn.decomposition import PCA
 
 
-def clean_data(df, path):
-    '''
-    Cleans the data from all the comments which are moderated
-    '''
 
-    # cmt = pd.read_csv("data/comments.csv")
-    cmt = pd.read_csv("../polis/openData/" + path + "/comments.csv")
-
-    cmt = cmt.loc[cmt['moderated'] == -1]
-    bad_cmt_id = cmt['comment-id'].values
-    rows_to_delete = []
-    for i in range(df.shape[0]):
-        cmt_id = df[i,0]
-        if cmt_id in bad_cmt_id:
-            rows_to_delete.append(i)
-    
-    cleaned_df = np.delete(df,bad_cmt_id, axis= 0)
-    print("n_cleaned cmt ", len(bad_cmt_id))
-
-    return cleaned_df
 
 
 
 def cluster_analysis(underlying_data, id): 
-    print(underlying_data)
 
-
+    # the max nr of cmts and pers 
     max_cmt = max(underlying_data[:,0]) +1
     max_per = max(underlying_data[:,1]) +1 
-    print(max_cmt, max_per)
-
 
     ## data[cmt,per]
     data = np.zeros([max_cmt , max_per])
@@ -61,16 +28,19 @@ def cluster_analysis(underlying_data, id):
 
     bool_cmt = np.zeros(max_cmt, dtype="bool")
     bool_per = np.zeros(max_per, dtype="bool")
-    counter = 0
+    
     scores_2 = []
     scores_3 = []
     scores_4 = []
     idx = []
+    cnt = 0
+    inc = 10
+    goal = 50
 
+    print("length ", underlying_data.shape[0])
 
-    print(underlying_data.shape)
     for row in underlying_data:
-        counter += 1 
+        cnt += 1 
         cmt_id = row[0]
         per_id = row[1]
         vote = row[2]
@@ -86,35 +56,70 @@ def cluster_analysis(underlying_data, id):
         d1 = data[bool_cmt,:]
         d2 = d1[:,bool_per]
 
+        
+        # print(bool_cmt)
 
-        if counter % 10 == 0 and counter != 0:
-            idx.append(counter)
-            labels_2 = rep.clustring(d2)
-            score_2 = silhouette_score(d2, labels_2)
-            scores_2.append(score_2)
-            # print(score_2)
+        # if (cnt == goal):
+        #     goal += inc
+        #     inc += 5
+            
+        #     idx.append(cnt)
+        #     labels_2 = rep.clustring(d2)
+        #     score_2 = silhouette_score(d2, labels_2)
+        #     scores_2.append(score_2)
+        #     # print(score_2)
 
-            labels_3 = rep.clustring(d2,3)
-            score_3 = silhouette_score(d2, labels_3)
-            scores_3.append(score_3)
+        #     labels_3 = rep.clustring(d2,3)
+        #     score_3 = silhouette_score(d2, labels_3)
+        #     scores_3.append(score_3)
             
             # labels_4 = rep.clustring(d2,4)
             # score_4 = silhouette_score(d2, labels_4)
             # scores_4.append(score_4)
 
-        if counter % 100 == 0:
+        if cnt % 1000 == 0:
             # update for the cmd 
-            print("index", counter)
+            print("index", cnt, " and inc ", inc)
         
 
-        if counter > 10000:
+        if cnt > 150000:
+
+
             break
 
 
-    plt.plot(idx,scores_2,  label=id + "2")
-    plt.plot(idx, scores_3, label=id + "3")
-    # plt.plot(scores_4, label='4')
+
+    red_data = PCA(n_components=2).fit_transform(d2)
+    labels_2 = rep.clustring(red_data)
+    print("1:", sum(labels_2)," 2:" , len(labels_2)-sum(labels_2))
+    s1 = silhouette_score(d2, labels_2)    
+    s2 = silhouette_score(red_data, labels_2)    
+    print("prepca:", s1, " postpca:",s2)
+
+
+    labels_2, best_n = rep.ideal_n_cluster(red_data)
+    print("NICE AMOUNT OF CLUSTER: ", best_n)
+
+
+
+    fig, ax = plt.subplots()
+    for g in np.unique(labels_2):
+        ix = np.where(labels_2 == g)
+        # if (g == 99 or g == 100):
+        #     ax.scatter(out[ix, 0], out[ix, 1], s=10, label=g)
+        # else:
+        ax.scatter(red_data[ix, 0], red_data[ix, 1], s=1, label=g)
+
     plt.legend()
+    plt.savefig("tmp/pca")
+    plt.close()
+
+   
+    # print(idx)
+    # plt.plot(idx,scores_2,  label=id + "2")
+    # plt.plot(idx, scores_3, label=id + "3")
+    # # plt.plot(scores_4, label='4')
+    # plt.legend()
 
     
 
@@ -126,16 +131,11 @@ def cluster_analysis(underlying_data, id):
 
 
 
-
-
-
 if __name__ == '__main__':
-    data, path = cc.preprossessing(True)
-    c_data = clean_data(data, path)
+    data, path = cc.preprossessing(True,False)
 
-    cluster_analysis(data,"dirt")
-    cluster_analysis(c_data, "clean")
-    plt.savefig("tmp/sil_cldasbkdsabd.png")
+    cluster_analysis(data,"cleaned_data")
+    # plt.savefig("tmp/sil_" + path+ ".pdf")
 
 
     pass
